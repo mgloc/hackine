3ed7ee1bb81c236bb73027393284ad721af7c767
gloxounet <mglock@outlook.fr>
2023-01-25 13:25:54 +0100

    requiremetns update + cors

--- services/backend/server.py
+++ services/backend/server.py
@@ -6,7 +6,7 @@ import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+socketio = SocketIO(app,cors_allowed_origins="*")
 thread = None
 
 def backgroundThread():
