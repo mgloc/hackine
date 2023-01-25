381cde72df281c68e67dd65e999bb691d07197c9
gloxounet <mglock@outlook.fr>
2023-01-25 17:50:14 +0100

    final commit

--- services/client/src/components/StatusBox/StatusBox.svelte
+++ services/client/src/components/StatusBox/StatusBox.svelte
@@ -1,55 +1,76 @@
 <script>
-	export let connexionOk = true
-	export let balancesOk = false 
+	export let connexionOk = true;
+	export let balancesOk = true;
+	export let balancesMainsOk = false;
 
-
-	const greentickUrl = "./images/greentick.svg"
-	const redcrossUrl = "./images/redcross.svg"
+	const greentickUrl = "./images/greentick.svg";
+	const redcrossUrl = "./images/redcross.svg";
 </script>
 
 <div class="main">
 	<h2>STATUS</h2>
 	<div class="content">
 		<div class="el">
-			<h3>Balances</h3>
-			<img src={balancesOk ? greentickUrl : redcrossUrl} alt="OK" width="16px">
+			<h3>Balances pied</h3>
+			<img
+				src={balancesOk ? greentickUrl : redcrossUrl}
+				alt="OK"
+				width="16px"
+			/>
 		</div>
-		
+
+		<div class="el">
+			<h3>Balances mains</h3>
+			<img
+				src={balancesMainsOk ? greentickUrl : redcrossUrl}
+				alt="OK"
+				width="16px"
+			/>
+		</div>
+
 		<div class="el">
 			<h3>Connexion</h3>
-			<img src={connexionOk ? greentickUrl : redcrossUrl} alt="OK" width="16px">
+			<img
+				src={connexionOk ? greentickUrl : redcrossUrl}
+				alt="OK"
+				width="16px"
+			/>
 		</div>
 	</div>
 </div>
 
 <style>
 	.main {
-		width:325px;
+		width: 325px;
 		text-align: center;
-		background: linear-gradient(180deg, rgba(217, 217, 217, 0.2) 0%, rgba(217, 217, 217, 0) 100%);
+		background: linear-gradient(
+			180deg,
+			rgba(217, 217, 217, 0.2) 0%,
+			rgba(217, 217, 217, 0) 100%
+		);
 		border-radius: 39px;
-		padding-top:10px;
-		padding-bottom:10px
+		padding-top: 10px;
+		padding-bottom: 10px;
 	}
 
 	h2 {
 		letter-spacing: 8pt;
-		font-size:16pt
+		font-size: 16pt;
 	}
 
 	.content {
-		margin:auto;
-		margin-top:10px;
-		width:80%;
+		margin: auto;
+		margin-top: 10px;
+		width: 80%;
 	}
 
 	.el {
-		display:flex;
+		display: flex;
 		justify-content: space-between;
 	}
 
 	.el h3 {
-		margin-top:0;
-		font-weight:400
+		margin-top: 0;
+		font-weight: 400;
 	}
 </style>
